83ded8a2ed0301c01325d08c67ad61aaab4686b8
Vinicius Abismael <vinicius.abismael@gmail.com>
2021-01-07 00:03:55 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/vinicius/cursomc/domain/Estado.java
+++ src/main/java/com/vinicius/cursomc/domain/Estado.java
@@ -1,7 +1,6 @@
 package com.vinicius.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
