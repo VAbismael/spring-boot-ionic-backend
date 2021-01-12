f9b75bc7cb6b264aa435465d5913369de501c4d1
Vinicius Abismael <vinicius.abismael@gmail.com>
2021-01-12 00:12:06 -0300

    Inserindo Pedido

--- src/main/java/com/vinicius/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/vinicius/cursomc/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.vinicius.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.vinicius.cursomc.domain.enums.EstadoPagamento;
 
 import javax.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
     private static final long serialVersionUID = 1L;
 
