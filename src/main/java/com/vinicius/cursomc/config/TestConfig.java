62f48b7290a96396b15a03eba3a7b39c0c5be7ac
Vinicius Abismael <vinicius.abismael@gmail.com>
2021-01-12 00:38:34 -0300

    Criando profile de teste

---
+++ src/main/java/com/vinicius/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.vinicius.cursomc.config;
+
+import com.vinicius.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
