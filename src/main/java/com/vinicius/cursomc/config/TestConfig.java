cc47679c64791bc95bafd9d4e7284d096cf63f7a
Vinicius Abismael <vinicius.abismael@gmail.com>
2021-01-13 02:12:47 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/vinicius/cursomc/config/TestConfig.java
+++ src/main/java/com/vinicius/cursomc/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.vinicius.cursomc.config;
 
 import com.vinicius.cursomc.services.DBService;
+import com.vinicius.cursomc.services.EmailService;
+import com.vinicius.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +21,10 @@ public class TestConfig {
     public boolean instantiateDatabase() throws ParseException {
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new MockEmailService();
     }
 }
